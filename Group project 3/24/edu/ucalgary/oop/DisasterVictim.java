5da369433c57bb42f2a7a7764c03ce94eaf30122
Hisada-VI <hisada.vi@gmail.com>
2024-02-11 17:26:36 -0700

    added some classes and initiated DisasterVictim Class

--- Group project 3/24/edu/ucalgary/oop/DisasterVictim.java
+++ Group project 3/24/edu/ucalgary/oop/DisasterVictim.java
@@ -1,3 +1,23 @@
 
 
 package edu.ucalgary.oop;
+
+class DisasterVictim
+{
+    /*Attributes*/
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String comments;
+    private int ASSIGNED_SOCIAL_ID;
+    private MedicalRecord[] medicalRecords;
+    private FamilyRelation[] familyConnections;
+    private String ENTRY_DATE;
+    private Supply[] personalBelongings;
+    private String gender;
+    private int counter;
+
+    /*Methods*/
+
+    
+}
--- Group project 3/24/edu/ucalgary/oop/Supply.java
+++ Group project 3/24/edu/ucalgary/oop/Supply.java
@@ -2,7 +2,7 @@
 
 package edu.ucalgary.oop;
 
-class FamilyRelation
+public class Supply 
 {
     
 }
