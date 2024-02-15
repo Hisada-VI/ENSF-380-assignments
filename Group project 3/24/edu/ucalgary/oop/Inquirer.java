0613738e5fc47e34c748844a3039c4b89fd2cf0d
OmarAhmd10 <omar.ahmed3@ucalgary.ca>
2024-02-15 10:39:56 -0700

    worked on Inquirer class

--- Group project 3/24/edu/ucalgary/oop/Inquirer.java
+++ Group project 3/24/edu/ucalgary/oop/Inquirer.java
@@ -1,6 +1,33 @@
 package edu.ucalgary.oop;
 
-public class Inquirer 
-{
+public class Inquirer
+ {
+    private String firstName;
+    private String lastName;
+    private String servicesPhoneNum;
+    private String info;
+
+    public Inquirer(String firstName, String lastName, String servicesPhoneNum, String info) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.servicesPhoneNum = servicesPhoneNum;
+        this.info = info;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getServicesPhoneNum() {
+        return servicesPhoneNum;
+    }
+
+    public String getInfo() {
+        return info;
+    }
         
 }
