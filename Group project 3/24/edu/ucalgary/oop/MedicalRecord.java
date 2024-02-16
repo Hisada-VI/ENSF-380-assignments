3e70248bc9070200e9d112ac807b40ae53530dc0
Hisada-VI <hisada.vi@gmail.com>
2024-02-15 22:58:44 -0700

    added exceptions for MedicalRecord.java

--- Group project 3/24/edu/ucalgary/oop/MedicalRecord.java
+++ Group project 3/24/edu/ucalgary/oop/MedicalRecord.java
@@ -2,6 +2,9 @@
 
 package edu.ucalgary.oop;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MedicalRecord
 {
     private Location location;
@@ -9,10 +12,20 @@ public class MedicalRecord
     private String dateOfTreatment;
 
 
-    public MedicalRecord (Location location, String treatmentDetails, String dateOfTreatment){
+    public MedicalRecord (Location location, String treatmentDetails, String dateOfTreatment) throws IllegalArgumentException
+    {
+        Pattern datePattern = Pattern.compile("^\\d{4}[-]{1}\\d{2}[-]{1}\\d{2}$");
+        Matcher dateMatcher = datePattern.matcher(dateOfTreatment);
+        if (dateMatcher.find())
+        {
+            this.dateOfTreatment = dateOfTreatment;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Date must be of Format YYYY-MM-DD");
+        }
         this.location=location;
         this.treatmentDetails=treatmentDetails;
-        this.dateOfTreatment=dateOfTreatment;
     }
     public Location getLocation(){
         return location;
@@ -34,7 +47,17 @@ public class MedicalRecord
         return dateOfTreatment;
     }
 
-    public void setDateOfTreatment(String dateOfTreatment){
-        this.dateOfTreatment=dateOfTreatment;
+    public void setDateOfTreatment(String dateOfTreatment) throws IllegalArgumentException
+    {
+        Pattern datePattern = Pattern.compile("^\\d{4}[-]{1}\\d{2}[-]{1}\\d{2}$");
+        Matcher dateMatcher = datePattern.matcher(dateOfTreatment);
+        if (dateMatcher.find())
+        {
+            this.dateOfTreatment = dateOfTreatment;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Date must be of Format YYYY-MM-DD");
+        }
     }
 }
